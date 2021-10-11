d540377ae12b621aa8e5b8ea8f5458058cc45881
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2021-10-11 22:20:24 +0330

    Serverless configuration

---
+++ terraform/production/variables.tf
@@ -0,0 +1,11 @@
+variable "environment_name" {
+    type = string
+    default = "production"
+}
+
+variable "project_name" {
+    type = string
+
+    ### TODO - Update with the require project name. E.g. "Housing-Production"
+    default = "YOUR PROJECT NAME HERE"
+}
