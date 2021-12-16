9bef0fec1670567c33251c8de1446a5d61eb2540
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2021-12-17 01:42:46 +0330

    test endpoint rollbacked

--- FinanceServicesApi/Startup.cs
+++ FinanceServicesApi/Startup.cs
@@ -143,7 +143,10 @@ namespace FinanceServicesApi
             services.AddScoped<IGenerateUrl<Asset>, AssetUrlGenerator>();
             services.AddScoped<IGenerateUrl<Person>, PersonUrlGenerator>();
             services.AddScoped<IGenerateUrl<GetContactDetailsResponse>, ContactDetailUrlGenerator>();
-            services.AddAutoMapper(typeof(Startup));
+            //services.AddAutoMapper(cnf =>
+            //{
+            //    cnf.AddProfile<AccountAutoMapperProfile>();
+            //});
         }
 
         private static void ConfigureLogging(IServiceCollection services, IConfiguration configuration)
