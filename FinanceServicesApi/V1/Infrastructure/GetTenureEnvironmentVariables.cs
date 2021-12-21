3da750a2a1282be49d38ba3e88bd22602bf51273
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2021-12-21 20:16:43 +0330

    Local token variable replaced by Input Authorization token

--- FinanceServicesApi/V1/Infrastructure/GetTenureEnvironmentVariables.cs
+++ FinanceServicesApi/V1/Infrastructure/GetTenureEnvironmentVariables.cs
@@ -1,11 +1,18 @@
 using System;
 using FinanceServicesApi.V1.Infrastructure.Interfaces;
 using Hackney.Shared.Tenure.Domain;
+using Microsoft.AspNetCore.Http;
 
 namespace FinanceServicesApi.V1.Infrastructure
 {
     public class GetTenureEnvironmentVariables : IGetEnvironmentVariables<TenureInformation>
     {
+        private readonly IHttpContextAccessor _context;
+
+        public GetTenureEnvironmentVariables(IHttpContextAccessor context)
+        {
+            _context = context;
+        }
         public Uri GetUrl()
         {
             string result = Environment.GetEnvironmentVariable("TENURE_API_URL") ?? string.Empty; ;
@@ -16,7 +23,7 @@ namespace FinanceServicesApi.V1.Infrastructure
 
         public string GetToken()
         {
-            string result = Environment.GetEnvironmentVariable("TENURE_API_TOKEN") ?? string.Empty; ;
+            string result = _context.HttpContext.Request.Headers["Authorization"]; //Environment.GetEnvironmentVariable("TENURE_API_TOKEN") ?? string.Empty; ;
             if (string.IsNullOrEmpty(result))
                 throw new Exception("Tenure api token shouldn't be null or empty.");
             return result;
