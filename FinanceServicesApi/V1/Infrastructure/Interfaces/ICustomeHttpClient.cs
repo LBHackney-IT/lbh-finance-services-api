fc8438b95ea849af77a2c056e46107147102808d
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2022-01-06 20:18:01 +0330

    Customize HttpClient

--- FinanceServicesApi/V1/Infrastructure/Interfaces/ICustomeHttpClient.cs
+++ FinanceServicesApi/V1/Infrastructure/Interfaces/ICustomeHttpClient.cs
@@ -8,13 +8,6 @@ namespace FinanceServicesApi.V1.Infrastructure.Interfaces
     public interface ICustomeHttpClient
     {
         public void AddAuthorization(AuthenticationHeaderValue headerValue);
-        public void AddHeader(HttpHeader<string, string> header);
         public Task<HttpResponseMessage> GetAsync(Uri uri);
-    }
-
-    public class HttpHeader<T, T1>
-    {
-        public string Name { get; set; }
-        public string Value { get; set; }
     }
 }
