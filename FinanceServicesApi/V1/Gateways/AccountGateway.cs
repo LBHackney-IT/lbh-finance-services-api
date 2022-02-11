2efd381bc0d1d1ce09405a532114c3fa70a36252
AnnaGolosova <golosowa.anna@gmail.com>
2022-02-11 11:49:53 +0300

    Commented unused code for now

--- FinanceServicesApi/V1/Gateways/AccountGateway.cs
+++ FinanceServicesApi/V1/Gateways/AccountGateway.cs
@@ -1,34 +1,35 @@
-using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Threading.Tasks;
-using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
 using FinanceServicesApi.V1.Domain.AccountModels;
 using FinanceServicesApi.V1.Factories;
 using FinanceServicesApi.V1.Gateways.Interfaces;
 using FinanceServicesApi.V1.Infrastructure.Entities;
 using FinanceServicesApi.V1.Infrastructure.Interfaces;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
 
 namespace FinanceServicesApi.V1.Gateways
 {
     public class AccountGateway : IAccountGateway
     {
         private readonly IDynamoDBContext _dynamoDbContext;
-        private readonly IAmazonDynamoDB _amazonDynamoDb;
+        //private readonly IAmazonDynamoDB _amazonDynamoDb;
         private readonly IHousingData<Account> _housingData;
 
         [ExcludeFromCodeCoverage]
-        public AccountGateway(IDynamoDBContext dynamoDbContext, IAmazonDynamoDB amazonDynamoDb,IHousingData<Account> housingData)
+        public AccountGateway(IDynamoDBContext dynamoDbContext, /*IAmazonDynamoDB amazonDynamoDb,*/ IHousingData<Account> housingData)
         {
             _dynamoDbContext = dynamoDbContext;
-            _amazonDynamoDb = amazonDynamoDb;
+            //_amazonDynamoDb = amazonDynamoDb;
             _housingData = housingData;
         }
 
         public async Task<Account> GetById(Guid id)
         {
             if (id == Guid.Empty)
-                throw new ArgumentException($"{nameof(id).ToString()} shouldn't be empty.");
+            {
+                throw new ArgumentException($"{nameof(id)} shouldn't be empty.");
+            }
 
             var result = await _dynamoDbContext.LoadAsync<AccountDbEntity>(id).ConfigureAwait(false);
 
@@ -38,7 +39,10 @@ namespace FinanceServicesApi.V1.Gateways
         public async Task<Account> GetByTargetId(Guid targetId)
         {
             if (targetId == Guid.Empty)
-                throw new ArgumentException($"{nameof(targetId).ToString()} shouldn't be empty.");
+            {
+                throw new ArgumentException($"{nameof(targetId)} shouldn't be empty.");
+            }
+
             return await _housingData.DownloadAsync(targetId).ConfigureAwait(false);
         }
 
