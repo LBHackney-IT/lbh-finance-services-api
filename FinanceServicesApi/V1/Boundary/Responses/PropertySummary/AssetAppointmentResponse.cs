8231726b7e52a34f60758d40f12a0ffa47a8cd42
golosowa.anna <golosowa.anna@gmail.com>
2022-02-12 15:16:28 +0300

    Change models structure after merge from development branch

--- FinanceServicesApi/V1/Boundary/Responses/PropertySummary/AssetAppointmentResponse.cs
+++ FinanceServicesApi/V1/Boundary/Responses/PropertySummary/AssetAppointmentResponse.cs
@@ -1,27 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Text.Json.Serialization;
 
 namespace FinanceServicesApi.V1.Boundary.Responses.PropertySummary
 {
-    // Hanna Holasava
-    // Once the PR will be merged, we will use Enum from FinanceServicesApi.V1.Boundary.Request.Enums namespace
-    // https://github.com/LBHackney-IT/lbh-finance-services-api/pull/64
-    [JsonConverter(typeof(JsonStringEnumConverter))]
-    public enum AssetType
-    {
-        Property,
-        Block,
-        Estate
-    }
-
-    [JsonConverter(typeof(JsonStringEnumConverter))]
-    public enum ChargeSubGroup
-    {
-        Actual,
-        Estimate
-    }
-
     public class AssetAppointmentResponse
     {
         public Guid AssetId { get; set; }
@@ -37,20 +18,5 @@ namespace FinanceServicesApi.V1.Boundary.Responses.PropertySummary
         public List<PropertyCostTotals> BlockCosts { get; set; }
 
         public List<PropertyCostTotals> PropertyCosts { get; set; }
-    }
-
-    // Hanna Holasava
-    // Once the PR will be merged, we will use Enum from FinanceServicesApi.V1.Boundary.Response namespace
-    // https://github.com/LBHackney-IT/lbh-finance-services-api/pull/64
-    public class ChargesTotalResponse
-    {
-        ///<example>50.5</example>
-        public decimal Amount { get; set; }
-
-        ///<example>2021</example>
-        public short Year { get; set; }
-
-        ///<example>Actual</example>
-        public ChargeSubGroup Type { get; set; }
     }
 }
