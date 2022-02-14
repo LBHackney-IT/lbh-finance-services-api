b8634b0ccac57df27b768cbb4f310980eeee6153
Anna <golosowa.anna@gmail.com>
2022-02-14 17:28:21 +0300

    Fixed build error (#70)

--- FinanceServicesApi/V1/Infrastructure/DetailedChargeForYear.cs
+++ FinanceServicesApi/V1/Infrastructure/DetailedChargeForYear.cs
@@ -1,4 +1,3 @@
-using FinanceServicesApi.V1.Boundary.Responses.PropertySummary;
 using FinanceServicesApi.V1.Domain.Charges;
 using FinanceServicesApi.V1.Infrastructure.Enums;
 
@@ -8,7 +7,7 @@ namespace FinanceServicesApi.V1.Infrastructure
     {
         public short Year { get; set; }
 
-        public ChargeSubGroup ChargeSubGroup { get; set; }
+        public ChargeSubGroup? ChargeSubGroup { get; set; }
 
         public string Type { get; set; }
 
@@ -18,7 +17,7 @@ namespace FinanceServicesApi.V1.Infrastructure
 
         public decimal Amount { get; set; }
 
-        public DetailedChargeForYear(DetailedCharges detailedCharges, short year, ChargeSubGroup chargeSubGroup)
+        public DetailedChargeForYear(DetailedCharges detailedCharges, short year, ChargeSubGroup? chargeSubGroup)
         {
             Year = year;
             ChargeSubGroup = chargeSubGroup;
