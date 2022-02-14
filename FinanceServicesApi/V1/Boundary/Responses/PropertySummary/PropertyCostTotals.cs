a37d0e8bb3caa30fd71ddd18a718f37b8b6efc96
AnnaGolosova <golosowa.anna@gmail.com>
2022-02-14 16:06:40 +0300

    Rename ChargeGroup to ChargeName

--- FinanceServicesApi/V1/Boundary/Responses/PropertySummary/PropertyCostTotals.cs
+++ FinanceServicesApi/V1/Boundary/Responses/PropertySummary/PropertyCostTotals.cs
@@ -5,7 +5,7 @@ namespace FinanceServicesApi.V1.Boundary.Responses.PropertySummary
     public class PropertyCostTotals
     {
         ///<example>Block Cleaning</example>
-        public string ChargeGroup { get; set; }
+        public string ChargeName { get; set; }
 
         public List<ChargesTotalResponse> Totals { get; set; }
     }
