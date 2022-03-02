4c7036082991272cdb0c443f2de53c0aa65b8530
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2022-03-02 16:43:55 +0330

    financial year amendment

--- FinanceServicesApi/V1/Factories/ResponseFactory.cs
+++ FinanceServicesApi/V1/Factories/ResponseFactory.cs
@@ -88,7 +88,9 @@ namespace FinanceServicesApi.V1.Factories
             List<Transaction> transactions,
             Asset asset)
         {
-            var firstMondayOfApril = new DateTime(DateTime.UtcNow.Year, 1, 1);
+            var firstMondayOfApril = new DateTime(DateTime.UtcNow.Year, 4, 1);
+            var financialYear = DateTime.UtcNow.Year + (DateTime.UtcNow > firstMondayOfApril ? 0 : -1);
+            var financialCurrentDate = new DateTime(financialYear, 1, 1);
             while (firstMondayOfApril.DayOfWeek != DayOfWeek.Monday)
             {
                 firstMondayOfApril = firstMondayOfApril.AddDays(1);
@@ -101,15 +103,15 @@ namespace FinanceServicesApi.V1.Factories
                     p.ChargedAmount - p.PaidAmount - p.HousingBenefitAmount) ?? 0;
             var wtc = charges?.Sum(p =>
                 p.DetailedCharges.Where(c =>
-                    c.EndDate >= DateTime.UtcNow &&
+                    c.EndDate >= financialCurrentDate &&
                     c.Type.ToLower() == "service" &&
                     c.Frequency.ToLower() == "weekly").Sum(c => c.Amount));
             var yrd = charges?.Sum(p =>
                 p.DetailedCharges.Where(c =>
-                    c.EndDate >= DateTime.UtcNow &&
+                    c.EndDate >= financialCurrentDate &&
                     c.Type.ToLower() == "rent" &&
                     c.Frequency.ToLower() == "weekly").Sum(c => c.Amount)) * 52;
-            var pty = transactions?.Where(p => p.TransactionDate.Year >= DateTime.UtcNow.Year).Sum(p => p.PaidAmount);
+            var pty = transactions?.Where(p => p.TransactionDate.Year >= financialYear).Sum(p => p.PaidAmount);
             return new PropertySummaryResponse
             {
                 CurrentBalance = account?.ConsolidatedBalance,
